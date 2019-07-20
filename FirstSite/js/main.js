06f3210b7180f5afa3be97dc98c906245a34948c
Lenarqa <lenar.valiev211@gmail.com>
2019-07-20 20:25:35 +0700

    Add First Site

---
+++ FirstSite/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+	$('.offer-content a').click(function(){
+		$('.triangles').css('width','300px');
+	});
+
+	$('.menu li a').click(function(){
+		$(this).toggleClass('active');
+	});
+});
